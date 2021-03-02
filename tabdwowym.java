11f710054d499fcca478498c57fcce54408cf3e8
JaromisUwU <77607725+JaromisUwU@users.noreply.github.com>
2021-03-02 10:18:53 +0100

    Add files via upload

--- tabdwowym.java
+++ tabdwowym.java
@@ -1,19 +1,19 @@
 public class tabdwowym{
     public static void main (String[] args){
         tablica();
-    
-    
+    }
+    private static void tabdwowym(){
         int[][] tab = new int [2][2];
         tab[0][0] = 1;
         tab[0][1] = 2;
         tab[1][1] = 3;
         tab[1][2] = 4;
 
-        for (int i = 0; i<tab.lenght; i++){
-            for(int j=0;j<tab[i].lenght;j++){
+        for (int i=0; i<tab.lenght; i++){
+            for(int j=0; j<tab[i].length; j++){
                 System.out.println(tab[i][j]);
             }
         }
     }
-}
+    
 }
