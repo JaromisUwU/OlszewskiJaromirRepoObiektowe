24e29160c0f9352ce15b8602fcea81d1104740ca
JaromisUwU <77607725+JaromisUwU@users.noreply.github.com>
2021-03-02 10:16:07 +0100

    zad z tablic 2d

--- tabdwowym.java
+++ tabdwowym.java
@@ -1,31 +1,19 @@
-class tabdwowym.java{
-    public static void main(String [] args){
-    //Commit: Przypomnienie jak tworzy sie tablice 1wymiarowa    
-        int [] tab = new int[5];
-        //przypisanie do pierwszego elementu tablicy [index = 0] wartosci 40
-        tab[0] = 40;
-        //tak nie mozna bo error wyskkauje ze stringa nie mozna se na inta zrobic
-            //tab[1] = "Jeden"; //error
-        //wyświetlenie elementow tablicy:
-            for(int i=0;i<tab.length()i++){
-                System.out.println(tab[i]);
-            }    
-        //commit: Przypomnienie jak robic tab 1wym.
-        int[][] tab2b = new int[5][2]; 
-        //commit: Stworzenie tablicy 2wymiarowej
-        //przypisanie wartosci do tab 2wym.
-        tab2d[0][1] = 50;
-        tab2d[2][1] = -200;
-        tab2d[4][0] = 444;
-            //System.out.println(tab2d.length); //dlugosc tablicy 2wym. 
-            //System.out.println(tab2d.length*tab2d[0].length); //ile elementow ma tablica w ogole    
-        for (int i=0;i<tab2d.length;i++){
-            for(int j=0;j<tab2d[i].length;j++){
-                System.out.println(tab2d[i][0]+" "+tab2d[i][j]);
+public class tabdwowym{
+    public static void main (String[] args){
+        tablica();
+    
+    
+        int[][] tab = new int [2][2];
+        tab[0][0] = 1;
+        tab[0][1] = 2;
+        tab[1][1] = 3;
+        tab[1][2] = 4;
+
+        for (int i = 0; i<tab.lenght; i++){
+            for(int j=0;j<tab[i].lenght;j++){
+                System.out.println(tab[i][j]);
             }
-            System.out.println("Koniec pętli ze zmienna j");
         }
-        System.out.println("Koniec pętli ze zmienną i");
-        //commit: wyswietlenie tablicy 2wym.
     }
+}
 }
