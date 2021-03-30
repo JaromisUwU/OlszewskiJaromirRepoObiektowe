cbeea7fd2bdd0d303bab22a42138b568093ced60
JaromisUwU <77607725+JaromisUwU@users.noreply.github.com>
2021-03-30 10:07:21 +0200

    wszystkie super fantastyczne niece ładne najlepsze amazing kozackie masne pliki w 1 foldeze

---
+++ Nowy folder/Kolo.java
@@ -0,0 +1,12 @@
+class Kolo{
+   public Float liczba_pi = "";
+   public String promien_kola ="";
+  
+
+   private pi(Float liczba_pi){
+        this.liczba_pi =3.1415; 
+
+        private promien(String promien_kola){
+        this.promien_kola = 10;
+
+      
---
+++ Nowy folder/Logowanie.java
@@ -0,0 +1,16 @@
+class Logowanie{
+    private String login = "";
+    private String haslo = "";
+   public Logowanie(String login, String hasło){
+        this.login = login; 
+        this.haslo = haslo;
+     
+        
+        public void siema(){
+    return ("login : "+login+" \n hasło :+hasło+" ");
+                    
+         @Override
+        public String toString(){
+            return hasło +"\n "+login;
+           } 
+   }
