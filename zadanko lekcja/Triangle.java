c89a77aeeff55a976b660104af568028069604c6
JaromisUwU <77607725+JaromisUwU@users.noreply.github.com>
2021-04-13 10:08:15 +0200

    dzis trójkąt więcej kodu

--- zadanko lekcja/Triangle.java
+++ zadanko lekcja/Triangle.java
@@ -1,12 +1,16 @@
 class Triangle {
-    public Triangle (int wysokosc, int podstawa) {
-        this.wysokosc = wysokosc;
-        this.podstawa = podstawa;
+   // public Triangle (int wysokosc, int podstawa) {
+       // this.wysokosc = wysokosc;
+       // this.podstawa = podstawa;
 
         //rownoboczny
-            @Override
-        public String toString(){
-            return wysokosc * podstawa / 2;
-        }
+         //  @Override
+       // public String toString(){
+           // return wysokosc * podstawa / 2;
+      //  }
+        //Konstruktor1 Trojkat normal
+       public Triangle(int pod,int wys){
+        System.out.println("Pole trojkata: " + (pod*wys/2));
+    }
     } 
 }
